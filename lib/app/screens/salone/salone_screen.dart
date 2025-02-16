df041622b0a6185b3c59edc9b7042859f7ed8fa0
Mehedi05739 <mehedi05739@gmail.com>
2025-02-16 23:47:20 +0600

    streggid view design implemented

--- lib/app/screens/salone/salone_screen.dart
+++ lib/app/screens/salone/salone_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:food_user_app/app/common/local_data.dart/category_food.dart';
 import 'package:food_user_app/app/common/widgets/custom_network_image.dart';
 import 'package:food_user_app/app/utils/font_size.dart';
@@ -6,6 +7,7 @@ import 'package:food_user_app/app/utils/images.dart';
 import 'package:food_user_app/app/utils/padding_size.dart';
 import 'package:food_user_app/app/utils/radius_size.dart';
 import 'package:food_user_app/app/utils/style.dart';
+
 class SaloneScreen extends StatefulWidget {
   const SaloneScreen({super.key});
 
@@ -79,7 +81,7 @@ class _SaloneScreenState extends State<SaloneScreen> {
                     },
                     child: AnimatedContainer(
                       duration: const Duration(milliseconds: 500),
-                      margin: const EdgeInsets.only(right: PaddingSize.small),
+                      margin: const EdgeInsets.only(right: PaddingSize.medium),
                       // padding: const EdgeInsets.all(1),
                       width: index == _selectedIndex ? 120 : 50,
                       decoration: BoxDecoration(
@@ -111,14 +113,147 @@ class _SaloneScreenState extends State<SaloneScreen> {
               )),
             ),
 
+            // SliverToBoxAdapter(
+            //   child: ListView.builder(
+            //     itemCount: CategoryFood.categories.length,
+            //     shrinkWrap: true,
+            //     itemBuilder: (context, index) {
+            //     return MasonryGridView.builder(
+            //         gridDelegate: SliverSimpleGridDelegateWithFixedCrossAxisCount(
+            //           crossAxisCount: 2, // Number of columns
+            //         ),
+            //         itemCount: CategoryFood.categories[index].foodItems.length, // Number of items
+            //         shrinkWrap: true,
+            //         physics: NeverScrollableScrollPhysics(),
+            //         itemBuilder: (context, index) {
+            //           return Container(
+            //             margin: EdgeInsets.all(8),
+            //             height: index%2 == 0 ? 250 : 170,
+            //             decoration: BoxDecoration(
+            //               color: Colors.amber,
+            //               // color: Colors.primaries[index % Colors.primaries.length],
+            //               borderRadius: BorderRadius.circular(26),
+            //             ),
+            //             child: ClipRRect(
+            //               borderRadius: BorderRadius.circular(26),
+            //               child: CustomNetworkImage(image: CategoryFood.categories[index].foodItems[index].image, height: 100, width: double.infinity, fit: BoxFit.cover),
+            //             ),
+            //           );
+            //         },
+            //       );
+            //   }),
+            // ),
+
             SliverList(
+              
               delegate: SliverChildBuilderDelegate(
+                childCount: CategoryFood.categories.length,
                 (BuildContext context, int index) {
-                  return ListTile(
-                    title: Text('Item $index'),
+                  // return ListTile(
+                  //   title: Text(CategoryFood.categories[index].name),
+                  // );
+
+                  return MasonryGridView.builder(
+                    gridDelegate: SliverSimpleGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2, // Number of columns
+                    ),
+                    itemCount: CategoryFood.categories[index].foodItems.length, // Number of items
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemBuilder: (context, i) {
+                      return Container(
+                        margin: EdgeInsets.all(8),
+                        height: i%2 == 0 ? 250 : 170,
+                        decoration: BoxDecoration(
+                          color: Colors.amber,
+                          // color: Colors.primaries[index % Colors.primaries.length],
+                          borderRadius: BorderRadius.circular(26),
+                          border: Border.all(color: Theme.of(context).disabledColor, width: 0.4),
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(26),
+                          child: CustomNetworkImage(image: CategoryFood.categories[index].foodItems[i].image, height: 100, width: double.infinity, fit: BoxFit.cover),
+                        ),
+                      );
+                    },
                   );
+
+                  // return MasonryGridView.count(
+                  //   crossAxisCount: 4,
+                  //   mainAxisSpacing: 4,
+                  //   crossAxisSpacing: 4,
+                  //   itemBuilder: (context, index) {
+                  //     return Container(
+                  //       height: (index % 3 + 1) * 100.0,
+                  //       color: Colors.blue,
+                  //     );
+                  //     // return Tile(
+                  //     //   index: index,
+                  //     //   extent: (index % 5 + 1) * 100,
+                  //     // );
+                  //   },
+                  // );
+
+                  // return StaggeredGrid.count(
+                  //   crossAxisCount: 3,
+                  //   mainAxisSpacing: 10,
+                  //   crossAxisSpacing: 10,
+                  //   children: CategoryFood.categories.map(
+                  //     (category) {
+                  //       int i = CategoryFood.categories.indexOf(category);
+                  //       return Container(
+                  //         height: i.isOdd ? 200 : 150,
+                  //         decoration: BoxDecoration(
+                  //           color: Theme.of(context).cardColor,
+                  //           borderRadius: BorderRadius.circular(RadiusSize.medium),
+                  //         ),
+                  //         child: Column(
+                  //           children: [
+                  //             ClipRRect(
+                  //               borderRadius: BorderRadius.circular(RadiusSize.medium),
+                  //               child: CustomNetworkImage(image: category.image, height: 100, width: double.infinity, fit: BoxFit.cover),
+                  //             ),
+
+                  //             Padding(
+                  //               padding: const EdgeInsets.all(PaddingSize.medium),
+                  //               child: Text(category.name, style: fontStyleNormal.copyWith(fontSize: FontSize.medium, color: Theme.of(context).cardColor)),
+                  //             ),
+                  //           ],
+                  //         ),
+                  //       );
+                  //     },
+                  //   ).toList(),
+                    
+                    // children: [
+                    //   StaggeredGridTile.count(
+                    //     crossAxisCellCount: 2,
+                    //     mainAxisCellCount: 3,
+                    //     child: Container(color: Colors.amber,),
+                    //   ),
+                    //   StaggeredGridTile.count(
+                    //     crossAxisCellCount: 2,
+                    //     mainAxisCellCount: 2,
+                    //     child: Container(color: Colors.green,),
+                    //   ),
+                    //   StaggeredGridTile.count(
+                    //     crossAxisCellCount: 1,
+                    //     mainAxisCellCount: 1,
+                    //     child: Container(color: Colors.blue,),
+                    //   ),
+                    //   StaggeredGridTile.count(
+                    //     crossAxisCellCount: 1,
+                    //     mainAxisCellCount: 1,
+                    //     child: Container(color: Colors.grey,),
+                    //   ),
+                    //   StaggeredGridTile.count(
+                    //     crossAxisCellCount: 4,
+                    //     mainAxisCellCount: 2,
+                    //     child: Container(color: Colors.deepOrange,),
+                    //   ),
+                    // ],
+                  // );
                 },
-                childCount: 100,
+              
               ),
             ),
           ],
